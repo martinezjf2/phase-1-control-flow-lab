6fcf150d7338bdeb8129ff05ce9ee57b53d916ed
Jeffrey Martinez <martinezjf2@gmail.com>
2021-09-26 18:10:10 -0400

    commit tree d7e5f7cc9b5f8d4d85d0023f2ae8350029d6adf3

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return "This one is on me!"
+  } else if (feet > 400 && feet <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(dest){
   // Write your code here!
+  return dest === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    case 'thanks for everything':
+      return 'Bye.'
+      break;
+  }
 }
